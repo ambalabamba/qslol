b9a328a6462203b9f66c067c455b59be57d7044d
ambalabamba <38856804+ambalabamba@users.noreply.github.com>
2019-04-09 18:53:45 +0200

    Update qs.py

--- qs.py
+++ qs.py
@@ -100,7 +100,6 @@ class QSBot:
         Handshake = self.sendPacket(self.SocketConn, '08HxO9TdCC62Nwln1P', True).strip(self.NullByte.decode('utf-8'))
 
         if Handshake == '08'
-            02A00LOL;G
             #02B100!lol
             Packets = ['02A00!lol;G', '0k1']
 
